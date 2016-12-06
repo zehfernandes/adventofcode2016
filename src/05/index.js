49cd7519c30ed2f56485899e2555d7983142a2e7
zehfernandes <ozehfernandes@gmail.com>
2016-12-06 00:02:22 -0200

    better performance time

--- src/05/index.js
+++ src/05/index.js
@@ -3,14 +3,17 @@ import MD5 from './MD5'
 const firstAnswer = (input) => {
   let code  = ""
   let count = 0
+  let i = 0
 
-  while (code.length < 8) {
+  while (i < 8) {
     const hash = `${input}${count}`
     const md5 = MD5(hash)
 
-    if(md5.substring(0,5) === "00000") {
+    if(md5.substring(0,5).localeCompare("00000") === 0) {
       let char = md5.charAt(5)
       code += char
+
+      i++
     }
 
     count++

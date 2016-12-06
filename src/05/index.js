6de90cfb2ba186ae9ae69e915ef5b05b8ec21013
zehfernandes <ozehfernandes@gmail.com>
2016-12-06 00:20:21 -0200

    Second answer

--- src/05/index.js
+++ src/05/index.js
@@ -3,7 +3,7 @@ import MD5 from './MD5'
 const firstAnswer = (input) => {
   let code  = ""
   let count = 0
-  let i = 0
+  let i = 0 //Performance option
 
   while (i < 8) {
     const hash = `${input}${count}`
@@ -24,6 +24,32 @@ const firstAnswer = (input) => {
 }
 
 const secondAnswer = (input) => {
+  let code  = [0,0,0,0,0,0,0,0]
+  let count = 0
+  let i = 0
+
+  while (i < 8) {
+    const hash = `${input}${count}`
+    const md5 = MD5(hash)
+
+    if(md5.substring(0,5) === "00000") {
+      let position = parseInt(md5.charAt(5))
+      console.log(position)
+      if (position <= 7) {
+        let char = md5.charAt(6)
+        code[position] = char
+
+        i++
+      }
+    }
+
+    count++
+  }
+
+  formatCode = ""
+  code.map((n) =>{ formatCode += n })
+
+  return formatCode
 
 }
 
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ fs.readFile('src/05/input.txt', 'utf8', (err, data) => {
     return
   }
 
-  console.time("firstAnswer")
-  console.log(`First Answer: ${firstAnswer(data)}`)
-  console.timeEnd("firstAnswer")
+  //console.time("firstAnswer")
+  //console.log(`First Answer: ${firstAnswer(data)}`)
+  //console.timeEnd("firstAnswer")
 
-  //console.time("secondAnswer")
-  //console.log(`Second Answer: ${secondAnswer(data)}`)
-  //console.timeEnd("secondAnswer")
+  console.time("secondAnswer")
+  console.log(`Second Answer: ${secondAnswer(data)}`)
+  console.timeEnd("secondAnswer")
 
 })
